25ed5cb03e33aed4811099ae4f1b5ea008631ff6
Evans-musamia <139111024+Evans-musamia@users.noreply.github.com>
2023-08-21 12:54:23 +0300

    latest commit august 21

--- oveschatbot_shopify.py
+++ oveschatbot_shopify.py
@@ -9,46 +9,44 @@ from langchain.vectorstores import Chroma
 from langchain.document_loaders import TextLoader
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.document_loaders import NotionDBLoader
+from langchain.document_loaders import DirectoryLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain import PromptTemplate
 from langchain.chains import ConversationChain
 from langchain.retrievers import TFIDFRetriever
-from docx import Document
+from getpass import getpass
 import os
 url = "https://oves-2022.myshopify.com/api/2023-04/graphql.json"
 page =1
 def get_json(url, page):
-    """
-    Get Shopify products.json from a store URL.
-    Args:
-        url (str): URL of the store.
-        page (int): Page number of the products.json.
-    Returns:
-        products_json: Products.json from the store.
-        """
     try:
         url = "https://oves-2022.myshopify.com/api/2023-04/graphql.json"
-
         payload = json.dumps({
           "query": "query Product($first: Int) {\n  products(first: $first) {\n    nodes {\n      id\n      title\n      handle\n      descriptionHtml\n      publishedAt\n      createdAt\n      updatedAt\n      vendor\n      tags\n      createdAt\n      \n    }\n  }\n}",
           "variables": {
             "first": 100
           }
         })
+
+
+        payloads = "{\"query\":\"query MyQuery {\\r\\n  articles(first: 100) {\\r\\n    nodes {\\r\\n      content\\r\\n    }\\r\\n  }\\r\\n}\",\"variables\":{}}"
         headers = {
           'Content-Type': 'application/json',
-          'X-Shopify-Storefront-Access-Token': 'access token',
-          'X-Shopify-Api-Version': 'version year'
+          'X-Shopify-Storefront-Access-Token': getpass(),
+          'X-Shopify-Api-Version': '2023-04'
         }
+
+        responses = requests.request("POST", url, headers=headers, data=payloads)
+        products_dicts = json.loads(responses.text[20:-2])
+
+        OUTPUT_DIR= os.getcwd()
+        f = open(os.path.join(OUTPUT_DIR, 'articles.csv'), 'w')
+        f.write('')
+        df = pd.DataFrame.from_dict(products_dicts['nodes'])
+        df.to_csv("articles.csv",index = True, header = True,encoding="utf8")
         response = requests.request("POST", url, headers=headers, data=payload)
         products_json = response.text[20:-2]
-        print(products_json)
-        '''try:
-        response = requests.get(f'{url}/products.json?limit=250&page={page}', timeout=5)
-        products_json = response.text
-        response.raise_for_status()
-        print(products_json)'''
         products_dict = json.loads(products_json)
         #print(products_dict)
         OUTPUT_DIR= os.getcwd()
@@ -56,8 +54,6 @@ def get_json(url, page):
         f.write('')
         df = pd.DataFrame.from_dict(products_dict['nodes'])
         df.to_csv("shopifyy_data.csv",index = True, header = True,encoding="utf8")
-        print(df)
-        
 
     except requests.exceptions.HTTPError as error_http:
         print("HTTP Error:", error_http)
@@ -72,52 +68,7 @@ def get_json(url, page):
         print("Error: ", error)
 
 get_json(url, page)
-products_json = get_json(url, 1)
-def to_df(products_json):
-    """
-    Convert products.json to a pandas DataFrame.
-    Args:
-        products_json (json): Products.json from the store.
-    Returns:
-        df: Pandas DataFrame of the products.json.
-    """
-
-    try:
-        products_dict = json.loads(products_json)
-        print(products_dict)
-        df = pd.DataFrame.from_dict(products_dict['nodes'])
-        df.to_csv("shopifyy_data.csv",index = True, header = True)
-        print(df)
-    except Exception as e:
-        print(e)
-to_df(products_json)
-def get_products(url):
-    """
-    Get all products from a store.
-    Returns:
-        df: Pandas DataFrame of the products.json.
-    """
-
-    results = True
-    page = 1
-    df = pd.DataFrame()
-    try:
-        while results:
-            products_json = get_json(url, page)
-            products_dict = to_df(products_json)
-
-            if len(products_dict) == 0:
-                break
-            else:
-                df = pd.concat([df, products_dict], ignore_index=True)
-                page += 1
-
-        df['url'] = f"{url}/nodes/" + df['handle']
-        print(df)
-    except Exception as e:
-        print(e)
-
-loader = CSVLoader('shopifyy_data.csv',encoding="utf8")
+loader = DirectoryLoader(os.getcwd(), glob="**/*.csv")
 doc = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=400)
 docs = text_splitter.split_documents(doc)
