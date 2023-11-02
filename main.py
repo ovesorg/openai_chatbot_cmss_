9211129890174b61d887c147dcb70ae1a5b8f76f
Evans-musamia <139111024+Evans-musamia@users.noreply.github.com>
2023-11-02 13:13:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,9 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENVIRONMENT = "eu-west4-gcp"
 
+index_name = "chatbot"
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
@@ -45,78 +48,54 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 def process_query(user_query: str):
-    # Generate a response using LangChain and ChatGPT/OpenAI
     user_query = user_query
     response = qa.run({"context": global_context, "history": "",
                       "question": user_query, "query": user_query})
     return response
-
-
-
+    
 @app.on_event("startup")
 async def startup_event():
-    # load_and_process_notion_data()
+    pinecone_retriever = Pinecone.from_existing_index(index_name, embeddings)
 
-    # Make these variables global so they can be accessed by the endpoint function
-    global prompt, qa, user_query
-    
-    pinecone_retriever = Pinecone.from_existing_index(
-        "chatbot", embeddings)
-
-
-    # Create a weighted average retriever that combines the results from both retrievers.
-    # retriever = EnsembleRetriever(retrievers=[elasticsearch_retriever, pinecone_retriever.as_retriever(
-    # )], weights=[1, 0])
-    # db = Chroma.from_documents([global_context], embeddings)
-    # llm = OpenAI(temperature=0.8, openai_api_key=OPENAI_API_KEY)
     llm = OpenAI(temperature=0.8, openai_api_key=OPENAI_API_KEY, model='gpt-4')
     retriever = pinecone_retriever.as_retriever()
-template = """
-You are here to assist clients who want information about our products. Combine chat history for the user together with his question and give a response that is considerate of his previous conversation and present question.
-Address each client based on their username when responding and dont be monotonous.
-Use the following context (delimited by <ctx></ctx>) and the chat history    (delimited by <hs></hs>) to answer the question. 
+    template = """
+    You are here to assist clients who want information about our products. Combine chat history for the user together with his question and give a response that is considerate of his previous conversation and present question.
+    Address each client based on their username when responding and dont be monotonous.
+    Use the following context (delimited by <ctx></ctx>) and the chat history    (delimited by <hs></hs>) to answer the question. 
 
 
-You are a domain-specific assistant for Oves. Please note that your responses should be based exclusively on our data, and you should not rely on external sources.
+    You are a domain-specific assistant for Oves. Please note that your responses should be based exclusively on our data, and you should not rely on external sources.
 
-<ctx>
-{context}
-</ctx>
-------
-<hs>
-{history}
-</hs>
-------
-{question}
+    <ctx>
+    {context}
+    </ctx>
+    ------
+    <hs>
+    {history}
+    </hs>
+    ------
+    {question}
 
-Answer:
-"""
-embeddings = OpenAIEmbeddings(model_name="gpt-4.0-turbo", openai_api_key=OPENAI_API_KEY)
-pinecone.init(
-    api_key=PINECONE_API_KEY,
-    environment=PINECONE_ENVIRONMENT,
-)
-index_name = "chatbot"
-docsearch = Pinecone.from_existing_index(index_name, embeddings)
-llm = OpenAI(temperature=0.8, openai_api_key=OPENAI_API_KEY)
-retriever = docsearch.as_retriever()
-prompt = PromptTemplate(
-    input_variables=["history", "context", "question"],
-    template=template,
-)
-qa = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type='stuff',
-    retriever=retriever,
-    verbose=0,
-    chain_type_kwargs={
-        "verbose": False,
-        "prompt": prompt,
-        "memory": ConversationBufferMemory(
-            memory_key="history",
-            input_key="question"),
-    }
-)
+    Answer:
+    """
+    prompt = PromptTemplate(
+        input_variables=["history", "context", "question"],
+        template=template,
+    )
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type='stuff',
+        retriever=retriever,
+        verbose=0,
+        chain_type_kwargs={
+            "verbose": False,
+            "prompt": prompt,
+            "memory": ConversationBufferMemory(
+                memory_key="history",
+                input_key="question"),
+        }
+    )
 
 
 @app.post("/query/")
@@ -126,8 +105,6 @@ async def get_response(query: str):
 
     response = qa.run({"query": query})
     return {"response": response}
-
-
 
 if __name__ == "__main__":
     import uvicorn
