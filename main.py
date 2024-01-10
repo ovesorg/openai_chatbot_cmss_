04d3aa5bf447bf58d43ecb944f3bdebec10c9ccc
unknown <sssliana38@gmail.com>
2024-01-10 10:43:29 +0300

    addition of googlesheet api file

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ from langchain.vectorstores import Pinecone
 import pinecone
 import secrets
 import json
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configure CORS (enable all origins, allow credentials, allow all methods, allow all headers)
 # Load environment variables from .env file
 load_dotenv()
 OUTPUT_DIR= os.getcwd()
@@ -121,24 +122,40 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        print(data,flush=True)
-        try:
-          response = qa.run(data)
-          await websocket.send_text(response)
-        except Exception as e:
-          # Handle the exception (e.g., log it)
-          print(f"Error: {str(e)}")
-          # Continue the loop to keep the connection alive
-          continue
+        if isinstance(data, dict) or (isinstance(data, str) and data.startswith('{') and data.endswith('}')):
+            print("This is feedback message",flush=True)
+        else:
+            print("This is user query")
+            try:
+              response = qa.run(data)
+              await websocket.send_text(response)
+            except Exception as e:
+              # Handle the exception (e.g., log it)
+              print(f"Error: {str(e)}")
+              # Continue the loop to keep the connection alive
+              continue
 
 
 @app.post("/query/")
 async def get_response(query: str):
     if not query:
-        return {"error": "Query not provided"}
+        raise HTTPException(status_code=400, detail="Query not provided")
+    if isinstance(query, dict) or (isinstance(query, str) and query.startswith('{') and query.endswith('}')):
+        print("This is feedback message",flush=True)
 
-    response = qa.run({"query": query})
-    return {"response": response}
+    else:
+        print("user query received")
+        print(query)
+        try:
+            response = qa.run({"query": query})
+            print(response, flush=True)
+            return {"response": response}
+        except Exception as e:
+            # Handle the exception (e.g., log it)
+            print(f"Error: {str(e)}")
+            # Customize the response message for your specific use case
+            raise HTTPException(status_code=400, detail="Make your question more specific")
+
 
 @app.post("/submit-form/")
 async def submit_form(user_query: str, bot_response: str,user_expected_response:str,user_rating:int):
