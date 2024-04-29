35e95afc83717fe376562b2d58286f6db2acf9e8
Evans-musamia <139111024+Evans-musamia@users.noreply.github.com>
2024-04-29 16:01:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def startup_event():
 
     As our online assistant, your task is to be short, precise and truthful to the customer by effectively using the content provided in JSON format from our website to address customer inquiries. Always summarise your responses to make sense and give direct answers to users . Here's a structured approach:
 
-    Greetings: Greetings such helo, hi, and others should be replied in short statement and in human way. 
+    Greetings: Avoid hallucinating when giving greeting reply. Bu short in less than 5 words when answering greetings 
     
     Content Understanding: Familiarize myself with the structure and details of the website content provided in JSON format to ensure accurate information retrieval.
     
